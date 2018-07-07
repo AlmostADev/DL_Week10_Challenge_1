1111e2ed038cc8892fceb3d94f2159b3bfd37325
Manuel Olmos <manuelalejandro.soto1@gmail.com>
2018-07-07 12:39:41 -0400

    Exercise 6 done

--- Ejercicio6.rb
+++ Ejercicio6.rb
@@ -5,14 +5,19 @@ require 'rack'
 
 class MiPrimeraWebApp
  def call(env)
-  [000, {}, []]
+    [
+        # Agregar un código de respuesta 200.
+        200, 
+        # Agregar en los Response Headers un Content-type de tipo text/html.
+        {'Content-Type' => 'text/html'}, 
+        # Agregar en el Response Body una etiqueta de párrafo que contenga un texto Lorem ipsum.
+        ['<p>Maecenas vestibulum ultrices est eget rhoncus. Aliquam lobortis nisl at nulla laoreet
+        gravida. Vestibulum eu dignissim felis. Nullam quis sollicitudin neque, id fringilla leo.
+        Pellentesque lobortis tincidunt ex sed posuere. Nunc sed pretium quam, vitae tristique leo.
+        Ut sodales in orci pulvinar viverra. Nulla facilisi. Ut sed neque tellus. Aliquam eu enim nec
+        tortor varius gravida tristique sit amet velit.</p>']
+    ]
  end
 end
 
 run MiPrimeraWebApp.new
-
-# Se pide:
-
-# Agregar un código de respuesta 200.
-# Agregar en los Response Headers un Content-type de tipo text/html.
-# Agregar en el Response Body una etiqueta de párrafo que contenga un texto Lorem ipsum.
