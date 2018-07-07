2e80629722e2f5b94f9c6f700e8adfcbd6435f0f
Manuel Olmos <manuelalejandro.soto1@gmail.com>
2018-07-07 11:54:27 -0400

    Exercises 1 and 2 done, Exercise 3 with some changes

--- Ejercicio3.rb
+++ Ejercicio3.rb
@@ -15,8 +15,27 @@ end
 # Se pide:
 
 # Crear una clase Car que herede de Vehicle
-# El constructor de Car, además de heredar las propiedades de Vehicle, debe incluir un contador de instancias de Car.
-# Crear un método de clase en Car que devuelva la cantidad de instancias.
+class Car < Vehicle
+  # El constructor de Car, además de heredar las propiedades de Vehicle,
+  # debe incluir un contador de instancias de Car.
+  def initialize(model, year)
+    # heredado de Vehicle
+    super
+    @@countInst += 1
+  end
+  # Crear un método de clase en Car que devuelva la cantidad de instancias.
+  def self.instances
+    @@countInst
+  end
+
+  def engine_start
+    # heredado de Vehicle
+    super
+    puts 'El motor se ha encendido!'
+  end
+end
+
+
 # El método engine_start heredado debe además imprimir 'El motor se ha encendido!'.
 # Instanciar 10 Cars.
 # Consultar la cantidad de instancias generadas de Car mediante el método de clase creado.
