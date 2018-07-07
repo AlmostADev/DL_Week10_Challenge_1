c2c6d903cb8eaf5554ea1be0be346d699545d079
Manuel Olmos <manuelalejandro.soto1@gmail.com>
2018-07-07 12:32:55 -0400

    Exercise 5 done

--- Ejercicio5.rb
+++ Ejercicio5.rb
@@ -1,11 +1,11 @@
 # Transformar la clase Herviboro en un módulo. Implementar el módulo en la clase Conejo mediante
 # Mixin para poder acceder al método dieta desde la instancia. Finalmente imprimir la definición
 # de Hervíboro.
-class Herviboro
-  @@definir = 'Sólo me alimento de vegetales!'
+module Herviboro
+  @definir = 'Sólo me alimento de vegetales!'
 
   def self.definir
-    @@definir
+    @definir
   end
 
   def dieta
@@ -19,7 +19,8 @@ class Animal
   end
 end
 
-class Conejo < Animal < Herviboro
+class Conejo < Animal
+  include Herviboro
   def initialize(name)
     @name = name
   end
@@ -28,6 +29,13 @@ end
 conejo = Conejo.new('Bugs Bunny')
 conejo.saludar
 conejo.dieta
-Herviboro.definir
+puts Herviboro.definir
 
 # Pregunta: ¿Por qué es mejor solución la implementación de Mixin que mediante Herencia de Herencia?
+
+# Lo bueno de los mixin contra la herencia multiple, es que te permite usar modulos (reutilizando otro codigo) tomando en cuenta
+# que los modulos pueden contener muchas clases y sub-modulos evitando hacer esto con la herencia simple
+# Conejo < Animal < Herviboro -> Herencia simple
+
+# Conejo < Animal
+# include Herbivoro -> Usando mixin y modulos
